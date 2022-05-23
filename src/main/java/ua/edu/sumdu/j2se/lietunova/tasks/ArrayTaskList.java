27ce138369a55a0d10157f2ea287096245c35ed1
LietunovaAnn <semeyka99@gmail.com>
2022-05-23 15:36:57 +0300

    change arrayList to array

--- src/main/java/ua/edu/sumdu/j2se/lietunova/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/lietunova/tasks/ArrayTaskList.java
@@ -1,32 +1,49 @@
 package ua.edu.sumdu.j2se.lietunova.tasks;
 
-import java.util.ArrayList;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
 
 public class ArrayTaskList {
-    ArrayList<Task> arrayTaskLists = new ArrayList<Task>();
+    private Task[] arrayTaskLists;
+    private int sizeArray;
 
 
     public void add(Task task) {
-        arrayTaskLists.add(task);
+        if (sizeArray == 0) {
+            arrayTaskLists = new Task[sizeArray + 1];
+            arrayTaskLists[0] = task;
+        } else {
+            arrayTaskLists = Arrays.copyOf(arrayTaskLists, arrayTaskLists.length + 1);
+            arrayTaskLists[arrayTaskLists.length - 1] = task;
+        }
+        sizeArray++;
     }
 
     public boolean remove(Task task) {
-        return arrayTaskLists.remove(task);
+        for (int i = 0; i < arrayTaskLists.length; i++) {
+            if (arrayTaskLists[i].equals(task)) {
+                arrayTaskLists = ArrayUtils.removeElement(arrayTaskLists, arrayTaskLists[i]);
+                sizeArray--;
+                return true;
+            }
+        }
+        return false;
     }
 
     public int size() {
-        return arrayTaskLists.size();
+        return sizeArray;
     }
 
     public Task getTask(int index) {
-        return arrayTaskLists.get(index);
+        return arrayTaskLists[index];
     }
 
     public ArrayTaskList incoming(int from, int to) {
         ArrayTaskList arrayIncoming = new ArrayTaskList();
-        for (int i = 0; i < arrayTaskLists.size(); i++) {
-            if (arrayTaskLists.get(i).getTime() > from && arrayTaskLists.get(i).getTime() < to) {
-                arrayIncoming.add(arrayTaskLists.get(i));
+        for (int i = 0; i < arrayTaskLists.length; i++) {
+            if (arrayTaskLists[i].getTime() > from && arrayTaskLists[i].getTime() < to) {
+                arrayIncoming.add(arrayTaskLists[i]);
             }
         }
         return arrayIncoming;
