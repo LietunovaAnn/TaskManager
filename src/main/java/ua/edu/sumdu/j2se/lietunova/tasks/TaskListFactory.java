734e3ac5fdf854b22640116d138bc6ca01f1f88f
Аня <semeyka99@gmail.com>
2022-06-12 00:02:59 +0300

    improve lab 5

--- src/main/java/ua/edu/sumdu/j2se/lietunova/tasks/TaskListFactory.java
+++ src/main/java/ua/edu/sumdu/j2se/lietunova/tasks/TaskListFactory.java
@@ -2,15 +2,10 @@ package ua.edu.sumdu.j2se.lietunova.tasks;
 
 public class TaskListFactory {
 
-    public static AbstractTaskList createTaskList(ListTypes.types type) throws Exception {
-
-        switch (type) {
-            case ARRAY:
-                return new ArrayTaskList();
-            case LINKED:
-                return new LinkedTaskList();
-            default:
-                throw new Exception("Unknown type");
+    public static AbstractTaskList createTaskList(ListTypes.types type) {
+        if (type == ListTypes.types.ARRAY) {
+            return new ArrayTaskList();
         }
+        return new LinkedTaskList();
     }
 }
